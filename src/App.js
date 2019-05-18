10c318fd734ba100df9650baa0bd148894c2539b
syoimin <syoimin@gmail.com>
2019-05-19 00:16:01 +0900

    html2canvas init

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import "semantic-ui-css/semantic.min.css";
 import CreateBunshun from "./CreateBunshun";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import Upload from "./Upload";
+import Html2Canvas from "./html2canvas";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={CreateBunshun}/>
-        <Route exact path="/upload" component={Upload}/>
+        <Route exact path="/" component={CreateBunshun} />
+        <Route exact path="/upload" component={Html2Canvas} />
       </Switch>
     </Router>
   );
