ae97c20ce178e30352ae55c35e5df645eba727ba
syoimin <syoimin@gmail.com>
2019-05-19 06:48:30 +0900

    push canvas to s3 image and json

--- src/html2canvas.js
+++ src/html2canvas.js
@@ -3,6 +3,8 @@ import Axios from 'axios';
 import './html2canvas.css';
 
 import html2canvas from 'html2canvas';
+import { Auth } from "aws-amplify";
+import { S3 } from "aws-sdk";
 
 export default class html2canvasComponent extends React.Component {
     constructor(props) {
@@ -38,11 +40,107 @@ export default class html2canvasComponent extends React.Component {
             // console.log("update" + i);
             // console.log(this.state.update);
 
-
             const resultCapture = await html2canvas(document.querySelector("#capture"))
+            const base64 = resultCapture.toDataURL("image/png")
+            const blob = this.Base64toBlob(base64);
+
+            this.uploadImage(blob, i);
+
         }
+
+        this.uploadJson(resultStatuses);
         // console.log(this.state.resultTweetData);
+        // const credentials = await Auth.currentCredentials();
+        // const essentialCredentials = Auth.essentialCredentials(credentials);
+
+        // const json = {
+        //         imageName: []
+        // }
+        // for (let i = 0; i < resultStatuses.length; i++) {
+        //     json.imageName.push(`upload/${credentials.data.IdentityId}-image${num}.png`);
+        // }
+        // const params = {
+        //     Body: JSON.stringify(json),
+        //     Bucket: "spajam2019",
+        //     Key: `uploadJson/${credentials.data.IdentityId}.json`,
+        // };
+
+        // this.s3Client = new S3({
+        //     apiVersion: "2006-03-01",
+        //     credentials: essentialCredentials
+        // });
+
+        // await this.s3Client.putObject(params, function (err, data) {
+        // });
     };
+
+    async uploadJson(resultStatuses) {
+
+        const credentials = await Auth.currentCredentials();
+        const essentialCredentials = Auth.essentialCredentials(credentials);
+
+        const json = {
+            imageName: [],
+        }
+        for (let i = 0; i < resultStatuses.length; i++) {
+            json.imageName.push(`upload/${credentials.data.IdentityId}-image${i}.png`);
+        }
+        const params = {
+            Body: JSON.stringify(json),
+            Bucket: "spajam2019",
+            Key: `uploadJson/${credentials.data.IdentityId}.json`,
+        };
+
+        this.s3Client = new S3({
+            apiVersion: "2006-03-01",
+            credentials: essentialCredentials
+        });
+
+        await this.s3Client.putObject(params, function (err, data) {
+        });
+
+    }
+
+    // Base64データをBlobデータに変換
+    Base64toBlob(base64) {
+        // カンマで分割して以下のようにデータを分ける
+        // tmp[0] : データ形式（data:image/png;base64）
+        // tmp[1] : base64データ（iVBORw0k～）
+        var tmp = base64.split(',');
+        // base64データの文字列をデコード
+        var data = atob(tmp[1]);
+        // tmp[0]の文字列（data:image/png;base64）からコンテンツタイプ（image/png）部分を取得
+        var mime = tmp[0].split(':')[1].split(';')[0];
+        //  1文字ごとにUTF-16コードを表す 0から65535 の整数を取得
+        var buf = new Uint8Array(data.length);
+        for (var i = 0; i < data.length; i++) {
+            buf[i] = data.charCodeAt(i);
+        }
+        // blobデータを作成
+        var blob = new Blob([buf], { type: mime });
+        return blob;
+    }
+
+    async uploadImage(data, num) {
+        console.log(num);
+        // s3へのアクセスの手順を残す
+        const credentials = await Auth.currentCredentials();
+        const essentialCredentials = Auth.essentialCredentials(credentials);
+        this.s3Client = new S3({
+            apiVersion: "2006-03-01",
+            credentials: essentialCredentials
+        });
+
+        console.log(data);
+
+        const params = {
+            Body: data,
+            Bucket: "spajam2019",
+            Key: `upload/${credentials.data.IdentityId}-image${num}.png`,
+        };
+        await this.s3Client.putObject(params, function (err, data) {
+        });
+    }
 
     render() {
         return (
